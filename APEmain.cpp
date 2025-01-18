4b82660e19a0a766e80fbc650ec310abb4b5b4b7
Qiancy1427 <qcy20231427@outlook.com>
2025-01-18 22:02:57 +0800

    20250118 set pixel

--- APEmain.cpp
+++ APEmain.cpp
@@ -1,18 +1,14 @@
 #include<bits/stdc++.h>
 #include<windows.h>
-#include"easyx.h"
 #include<conio.h>
 #include"APEFL.h"
 
 using namespace std;
 
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, char *lpcmdline, int nShowcmd){
+    return MessageBox(NULL, "hello, world", "caption", 0);
+}
+
 int main(){
-	getch();
-    initgraph(1024, 512);
-    getch();
-    getch();
-    circle(320, 240, 100);
-    getch();
-    closegraph();
     return 0;
 }
