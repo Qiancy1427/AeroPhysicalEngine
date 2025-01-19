46aaccd4ce6a4f1d487dc8c86a99d0238538baa7
stamenst <stamenst.g@gmail.com>
2025-01-19 21:07:19 +0800

    fuckyou

--- APEFL.h
+++ APEFL.h
@@ -1,11 +1,69 @@
 #include<bits/stdc++.h>
-#include<windows.h>
-#include<conio.h>
+#include<sys/time.h>
+
+#include<Windows.h>
+#include<Mmsystem.h>
+#include<Prnasnot.h>
+#include<T2embapi.h>
+#include<Tvout.h>
+#include<Windef.h>
+#include<Windowsx.h>
+#include<Wingdi.h>
+#include<Winuser.h>
+
 
 #ifndef APEFL   
-#define APEFL  1  
+#define APEFL  1
+
 
 //classify vector starting
+class Vector2 {//classify Vector2
+	public:
+	    Vector2();
+	    Vector2(float a, float b);
+	    Vector2 operator+ (Vector2 v1);
+	    Vector2 operator- (Vector2 v1);
+	    float operator* (Vector2 v1);
+	    Vector2 operator^ (float k);
+	    static void print(Vector2 v1);
+	    float x, y;
+	};
+	Vector2::Vector2() {//empty Vector2 
+	    x = 0;
+	    y = 0;
+	}
+	Vector2::Vector2(float a, float b) {//Vector2 container 
+	    x = a;
+	    y = b;
+	}
+	Vector2 Vector2::operator+(Vector2 v1) {//Vector2 addtion 
+	    Vector2 vt;
+	    vt.x = vt.x + v1.x;
+	    vt.y = vt.y + v1.y;
+	    return vt;
+	}
+	Vector2 Vector2::operator-(Vector2 v1) {//Vector2 subtraction
+	    Vector2 vt;
+	    vt.x = vt.x - v1.x;
+	    vt.y = vt.y - v1.y;
+	    return vt;
+	}
+	float Vector2::operator*(Vector2 v1) {//Vector2 dot production
+	    float tmp;
+	    Vector2 vt;
+	    tmp = vt.x * v1.x + vt.y * v1.y;
+	    return tmp;
+	}
+	Vector2 Vector2::operator^(float k) {//Vector2 scalar multiplication
+	    Vector2 vt;
+	    vt.x = vt.x * k;
+	    vt.y = vt.y * k;
+	    return vt;
+	}
+	void Vector2::print(Vector2 v1) {//print Vector2
+	    std::cout << "(" << v1.x << "," << v1.y << ")\n";
+	}
+	
 	class Vector3 {//classify Vector3
 	public:
 	    Vector3();
@@ -27,38 +85,38 @@
 	    y = b;
 	    z = c;
 	}
-	Vector3 Vector3::operator+(Vector3 v1) {//addtion 
+	Vector3 Vector3::operator+(Vector3 v1) {//Vector3 addtion 
 	    Vector3 vt;
 	    vt.x = vt.x + v1.x;
 	    vt.y = vt.y + v1.y;
 	    vt.z = vt.z + v1.z;
 	    return vt;
 	}
-	Vector3 Vector3::operator-(Vector3 v1) {//subtraction
+	Vector3 Vector3::operator-(Vector3 v1) {//Vector3 subtraction
 	    Vector3 vt;
 	    vt.x = vt.x - v1.x;
 	    vt.y = vt.y - v1.y;
 	    vt.z = vt.z - v1.z;
 	    return vt;
 	}
-	float Vector3::operator*(Vector3 v1) {//��� 
+	float Vector3::operator*(Vector3 v1) {//Vector3 dot production
 	    float tmp;
 	    Vector3 vt;
 	    tmp = vt.x * v1.x + vt.y * v1.y + vt.z * v1.z;
 	    return tmp;
 	}
-	Vector3 Vector3::operator^(float k) {//���� 
+	Vector3 Vector3::operator^(float k) {//Vector3 scalar multiplication
 	    Vector3 vt;
 	    vt.x = vt.x * k;
 	    vt.y = vt.y * k;
 	    vt.z = vt.z * k;
 	    return vt;
 	}
-	void Vector3::print(Vector3 v1) {//������� 
+	void Vector3::print(Vector3 v1) {//print Vector3
 	    std::cout << "(" << v1.x << "," << v1.y << "," << v1.z << ")\n";
 	}
 	
-	class Vector4 {//����������� 
+	class Vector4 {//classify Vector4
 	public:
 	    Vector4();
 	    Vector4(float a, float b, float c, float d);
@@ -66,22 +124,22 @@
 	    static Vector3 normalize(Vector4 v1);
 	    float x, y, z, w;
 	};
-	Vector4::Vector4() {//�½������� 
+	Vector4::Vector4() {//empty Vector4 
 	    x = 0;
 	    y = 0;
 	    z = 0;
 	    w = 1;
 	}
-	Vector4::Vector4(float a, float b, float c, float d) {//�������� 
+	Vector4::Vector4(float a, float b, float c, float d) {//Vector4 container 
 	    x = a;
 	    y = b;
 	    z = c;
 	    w = d;
 	}
-	void Vector4::print(Vector4 v1) {//������� 
+	void Vector4::print(Vector4 v1) {//print Vector4
 	    std::cout << "(" << v1.x << "," << v1.y << "," << v1.z << "," << v1.w << ")\n";
 	}
-	Vector3 Vector4::normalize(Vector4 v1) {//��һ��������� 
+	Vector3 Vector4::normalize(Vector4 v1) {//Vector4 narmalization
 		Vector3 vt;
 		vt.x=v1.x/v1.w;
 		vt.y=v1.y/v1.w;
@@ -110,10 +168,31 @@ NBTs' categories format
  Acceleration 3
  Force 4
 */
-//HWND initgraph(int width, int height, int flag = 0);		// Create graphics window
-//void closegraph();
+
+
+const COLORREF rgbRed   =  0x000000FF;
+const COLORREF rgbGreen =  0x0000FF00;
+const COLORREF rgbBlue  =  0x00FF0000;
+const COLORREF rgbBlack =  0x00000000;
+const COLORREF rgbWhite =  0x00FFFFFF;
+
+COLORREF rgbcustom(short r,short g,short b){
+	COLORREF rgbt;
+	rgbt = (int)(r+g*256+b*256*256); 
+	
+	return rgbt;
+} 
+
+void paintrect(HDC hdc,Vector2 poslt,Vector2 posrb,COLORREF color){
+	for(int i=round(poslt.y);i<=round(posrb.y);i++){
+		for(int j=round(poslt.x);j<=round(posrb.x);j++){
+			SetPixel(hdc,j,i,color);
+			
+		}
+		
+	}
+	return;
+}
+
 
 #endif
-
-
-
--- APEmain.cpp
+++ APEmain.cpp
@@ -3,19 +3,113 @@
 #include<winuser.h>
 #include<wingdi.h>
 #include<conio.h>
+
 #include"APEFL.h"
 
 using namespace std;
 
-//int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, char *lpcmdline, int nShowcmd){
-//    return MessageBox(NULL, L"hello world", L"caption", 0);
-//}
 
-int main(){
-#ifdef UNICODE
-    cout << L"UNICODE";
-#else
-    cout << "UNICODE";
-#endif
+struct timeval previoustime,presenttime;
+int deltatime=0;
+int pos=0;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	previoustime.tv_sec=0;
+	previoustime.tv_usec=0;
+	presenttime.tv_sec=0;
+	presenttime.tv_usec=0;
+    // Register the window class.
+    const wchar_t CLASS_NAME[]  = L"APEwindow";
+    
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"APEmain",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        -10, -3, 1920, 1080,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        hInstance,  // Instance handle
+        NULL        // Additional application data
+        );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+    	
+        gettimeofday(&presenttime, NULL);
+		deltatime = (presenttime.tv_sec*1000)+(presenttime.tv_usec/1000)-(previoustime.tv_sec*1000)-(previoustime.tv_usec/1000);
+		previoustime = presenttime;
+		if(deltatime<1000){
+			pos=deltatime+pos;
+			cout<<"deltatime:"<<deltatime<<"ms\n";
+			cout<<pos<<"\n";
+	        UpdateWindow(hwnd);
+	        TranslateMessage(&msg);
+	        DispatchMessage(&msg);
+			
+		}
+        
+    }
+    
     return 0;
 }
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+            
+            // All painting occurs here, between BeginPaint and EndPaint.
+
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+			paintrect(hdc,Vector2(pos,150),Vector2(pos+50,200),rgbcustom(255,0,0));
+
+            EndPaint(hwnd, &ps);
+            
+    		
+    		
+        }
+        return 0;
+
+    }
+    
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+
+
